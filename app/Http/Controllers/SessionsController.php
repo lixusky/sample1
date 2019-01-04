9eff65dc386b06d835fd7107dc3281588982722a
lixu <812486428@qq.com>
2019-01-04 17:18:12 +0000

    Finish login and logout

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -19,16 +19,23 @@ class SessionsController extends Controller
     		'password'=>'required'
     	]);
 
-    	if(Auth::attempt($credentials))
+    	if(Auth::attempt($credentials,$request->has('remember')))
     	{
-    		session()-flash('success','欢迎回来！');
+    		session()->flash('success','欢迎回来！');
     		return redirect()->route('users.show',[Auth::user()]);
     	}
     	else
     	{
-    		session()-flash('danger','很抱歉，你的邮箱和密码不匹配！');
+    		session()->flash('danger','很抱歉，你的邮箱和密码不匹配！');
     		return redirect()->back();
     	}
     	return;
+    }
+
+    public function destroy()
+    {
+    	Auth::logout();
+    	session()->flash('success','您已经成功退出！');
+    	return redirect('login');
     }
 }
